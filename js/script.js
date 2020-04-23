95363de4fbf24195ceed4dbaeaa034459595ca69
kiitoss <antoinechataignier1998@orange.fr>
2020-04-23 19:06:30 +0200

    feat: make data editable on click

--- js/script.js
+++ js/script.js
@@ -1,75 +1,92 @@
 var choix_type_colis = ["carton bois", "carton beige", "IFCO"];
-
 var status_creation_commande = new Object();
+status_creation_commande.id_commande = 0;
 status_creation_commande.estOuvert = false;
 status_creation_commande.nb_colis = null;
+status_creation_commande.id_colis = null;
+status_creation_commande.nb_palettes = null;
 
-function ajout_commande(quantite=1) {
+function ajout_commande() {
     // Ajoute une nouvelle ligne pour une nouvelle commande.
-    for (let i = 0; i < quantite; i++) {
-        let main_part = document.getElementById("main");;
-        let inserer_valider = document.getElementById("inserer_valider");
+    let main_part = document.getElementById("main");;
+    let inserer_valider = document.getElementById("inserer_valider");
 
-        let nouvelle_ligne = document.createElement("div");
-        nouvelle_ligne.setAttribute("class", "ligne_commande");
-
-        let colis_infos = document.createElement("div");
-        colis_infos.setAttribute("class", "colis_infos");
-
-        let label_colis = document.createElement("label");
-        label_colis.setAttribute("for", "input_colis");
-        label_colis.innerHTML = "Colis: ";
+    let nouvelle_ligne = document.createElement("div");
+    nouvelle_ligne.setAttribute("class", "ligne_commande");
+    nouvelle_ligne.setAttribute("id", "ligne_commande_"+status_creation_commande.id_commande);
 
 
-        let input_colis = document.createElement("input");
-        input_colis.setAttribute("type", "number");
-        input_colis.setAttribute("id", "input_colis");
-        input_colis.setAttribute("name", "input_colis");
-        input_colis.setAttribute("min", "0");
-        input_colis.setAttribute("max", "500");
-        input_colis.setAttribute("size", "1");
+    let colis_infos = document.createElement("div");
+    colis_infos.setAttribute("class", "colis_infos");
 
-        colis_infos.appendChild(label_colis);
-        colis_infos.appendChild(input_colis);
+    let label_colis = document.createElement("label");
+    label_colis.setAttribute("for", "input_colis");
+    label_colis.innerHTML = "Colis: ";
 
-
-        let cagette_infos = document.createElement("div");
-        cagette_infos.setAttribute("class", "cagette_infos");
-
-        let label_cagette = document.createElement("label");
-        label_cagette.setAttribute("for", "input_cagette");
-        label_cagette.innerHTML = "Type cagette: ";
-
-
-        let input_cagette = document.createElement("input");
-        input_cagette.setAttribute("type", "text");
-        input_cagette.setAttribute("id", "input_cagette");
-        input_cagette.setAttribute("name", "input_cagette");
-        input_cagette.setAttribute("maxlength", "50");
-        input_cagette.setAttribute("size", "10");
-
-        cagette_infos.appendChild(label_cagette);
-        cagette_infos.appendChild(input_cagette);
-
-
-        let palette_infos = document.createElement("div");
-        palette_infos.setAttribute("class", "palette_infos");
-
-        let btn_palette = document.createElement("button");
-        btn_palette.innerHTML = "=";
-
-        palette_infos.appendChild(btn_palette);
-
-
-
-        nouvelle_ligne.appendChild(colis_infos);
-        nouvelle_ligne.appendChild(cagette_infos);
-        nouvelle_ligne.appendChild(palette_infos);
-
-        main_part.insertBefore(nouvelle_ligne, inserer_valider);
+    let input_colis = document.createElement("input");
+    input_colis.setAttribute("type", "number");
+    input_colis.setAttribute("name", "input_colis");
+    input_colis.setAttribute("value", status_creation_commande.nb_colis);
+    input_colis.setAttribute("id", "nb_colis_commande_"+status_creation_commande.id_commande);
+    input_colis.setAttribute("id_commande", status_creation_commande.id_commande);
+    input_colis.setAttribute("size", "1");
+    input_colis.onclick = function() {
+        creation_commande_p1(this.getAttribute("id_commande"));
     }
+
+    colis_infos.appendChild(label_colis);
+    colis_infos.appendChild(input_colis);
+
+
+
+    let cagette_infos = document.createElement("div");
+    cagette_infos.setAttribute("class", "cagette_infos");
+
+    let label_cagette = document.createElement("label");
+    label_cagette.setAttribute("for", "input_cagette");
+    label_cagette.innerHTML = "Type cagette: ";
+
+    let input_cagette = document.createElement("input");
+    input_cagette.setAttribute("type", "text");
+    input_cagette.setAttribute("name", "input_cagette");
+    input_cagette.setAttribute("value", choix_type_colis[status_creation_commande.id_colis]);
+    input_cagette.setAttribute("id", "type_cagette_commande_"+status_creation_commande.id_commande);
+    input_cagette.setAttribute("id_commande", status_creation_commande.id_commande);
+    input_cagette.setAttribute("size", "10");
+    input_cagette.onclick = function() {
+        creation_commande_p2(this.getAttribute("id_commande"));
+    }
+
+    cagette_infos.appendChild(label_cagette);
+    cagette_infos.appendChild(input_cagette);
+
+
+    let palette_infos = document.createElement("div");
+    palette_infos.setAttribute("class", "palette_infos");
+
+    let btn_palette = document.createElement("button");
+    btn_palette.innerHTML = status_creation_commande.nb_palettes;
+    btn_palette.setAttribute("id", "nb_palettes_commande_"+status_creation_commande.id_commande);
+    btn_palette.setAttribute("id_commande", status_creation_commande.id_commande);
+    btn_palette.onclick = function() {
+        creation_commande_p3(this.getAttribute("id_commande"));
+    }
+
+    palette_infos.appendChild(btn_palette);
+
+
+
+    nouvelle_ligne.appendChild(colis_infos);
+    nouvelle_ligne.appendChild(cagette_infos);
+    nouvelle_ligne.appendChild(palette_infos);
+
+    main_part.insertBefore(nouvelle_ligne, inserer_valider);
+
+    status_creation_commande.id_commande += 1;
 }
 
+
+// Valide ou annule l'ajout d'une nouvelle commande.
 function terminer_ajout(ajout_valide) {
     if (ajout_valide) {
         ajout_commande();
@@ -78,15 +95,23 @@ function terminer_ajout(ajout_valide) {
     document.getElementById("ajouter_commande").style.display = "none";
     status_creation_commande.estOuvert = false;
     status_creation_commande.nb_colis = null;
+    status_creation_commande.id_colis = null;
+    status_creation_commande.nb_palettes = null;
 }
 
-function creation_commande_p1() {
+function creation_nouvelle_commande() {
+    creation_commande_p1();
+}
+
+// Création de l'interface pour choisir le nombre de colis de la commande.
+function creation_commande_p1(commande_id=null) {
     // Initialisation
     if (!status_creation_commande.estOuvert) {
         document.getElementById("ajouter_commande").style.display = "block";
+        status_creation_commande.estOuvert = !status_creation_commande.estOuvert;
     }
+    document.getElementById("ac_bouton_valider").style.display = "block";
     document.getElementById("ac_main_ajout_commande").innerHTML = "";
-
     document.getElementById("ac_titre_ajout_commande").innerHTML = "Nombre de colis";
 
     // Création de l'input pour entrer le nombre de colis.
@@ -100,67 +125,52 @@ function creation_commande_p1() {
     input_colis.setAttribute("maxlength", "3");
     input_colis.setAttribute("oninput", "check_valide_input_colis(this)");
     input_colis.setAttribute("size", "1");
+
     main_part.appendChild(input_colis);
 
-    if (status_creation_commande.nb_colis != null) {
+    if (commande_id != null) {
+        document.getElementById("ac_input_colis").value = document.getElementById("nb_colis_commande_"+commande_id).value;
+    }
+    else if (status_creation_commande.nb_colis != null) {
         document.getElementById("ac_input_colis").value = status_creation_commande.nb_colis;
     }
-
     document.getElementById("ac_input_colis").focus()
 
-    // Ecriture dans le bouton ac_bouton_suivant et affectation de la valeure onclick.
-    document.getElementById("ac_bouton_suivant").innerHTML = "suivant";
-    document.getElementById("ac_bouton_suivant").setAttribute("onclick", "creation_commande_p2()");
+    // Affectation de la valeur onclick au bouton ac_bouton_valider.
+    document.getElementById("ac_bouton_valider").onclick = function() {
+        // Récupération et stockage du nombre de colis rentrés.
+        let nb_colis = document.getElementById("ac_input_colis").value;
+        if (nb_colis.length == 0) {
+            nb_colis = 0;
+        }
+
+        // Si la commande est créé, le programme continue sur la prochaine étape,
+        // sinon il met à jour les données de la ligne de la commande.
+        if (commande_id == null) {
+            status_creation_commande.nb_colis = parseInt(nb_colis);
+            creation_commande_p2();
+        }
+        else {
+            document.getElementById("nb_colis_commande_"+commande_id).value = parseInt(nb_colis);
+            terminer_ajout(false);
+        }
+    }
 
     // Affectation de la valeur onclick du bouton ac_bouton_retour_arriere.
     document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "terminer_ajout(false)");
+
+    if (commande_id == null) {
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler création";
+    }
+    else {
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler";
+    }
 }
 
-function creation_commande_p2() {
-    // Récupération et stockage du nombre de colis rentrés.
-    let nb_colis = document.getElementById("ac_input_colis").value;
-    if (nb_colis.length == 0) {
-        nb_colis = 0;
-    }
-    status_creation_commande.nb_colis = parseInt(nb_colis);
-
-    // Initialisation.
-    document.getElementById("ac_main_ajout_commande").innerHTML = "";
-    document.getElementById("ac_titre_ajout_commande").innerHTML = "Type de colis";
-
-    // Création de la liste de choix.
-    let main_part = document.getElementById("ac_main_ajout_commande");
-    for (let i=0; i<choix_type_colis.length; i++) {
-        let nouveau_colis = document.createElement("div");
-        nouveau_colis.setAttribute("class", "ac_LC_colis");
-        let case_colis = document.createElement("input");
-        case_colis.setAttribute("type", "radio");
-        case_colis.setAttribute("name", "colis");
-        case_colis.setAttribute("class", "ac_case_colis");
-        case_colis.setAttribute("id", choix_type_colis[i]);
-
-        let nom_colis = document.createElement("label");
-        nom_colis.setAttribute("for", choix_type_colis[i]);
-        nom_colis.setAttribute("class", "ac_label_colis");
-        nom_colis.innerHTML = choix_type_colis[i];
-    
-        nouveau_colis.appendChild(case_colis);
-        nouveau_colis.appendChild(nom_colis);
-    
-        main_part.appendChild(nouveau_colis);
-    }
-
-    // Affectation de la valeur onclick du bouton ac_bouton_retour_arriere.
-    document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "creation_commande_p1()");
-
-    
-}
-
-
-
-
-
-
+// Vérifie le conditions lors de la saisie d'un nombre de colis dans l'input.
+//      -> nombre positif,
+//      -> nombre < 500,
+//      -> pas plus de 3 caractères.
 function check_valide_input_colis(objet) {
     objet.value = parseInt(objet.value);
     let minimum = parseInt(objet.min);
@@ -175,3 +185,120 @@ function check_valide_input_colis(objet) {
         objet.value = maximum;
     }
 }
+
+
+// Création de l'interface pour choisir le type de colis de la commande.
+function creation_commande_p2(commande_id=null) {
+    // Initialisation.
+    if (!status_creation_commande.estOuvert) {
+        document.getElementById("ajouter_commande").style.display = "block";
+        status_creation_commande.estOuvert = !status_creation_commande.estOuvert;
+    }
+    document.getElementById("ac_bouton_valider").style.display = "none";
+    document.getElementById("ac_main_ajout_commande").innerHTML = "";
+    document.getElementById("ac_titre_ajout_commande").innerHTML = "Type de colis";
+
+    // Création de la liste de choix.
+    let main_part = document.getElementById("ac_main_ajout_commande");
+    for (let i=0; i<choix_type_colis.length; i++) {
+        let nouveau_colis = document.createElement("div");
+        nouveau_colis.setAttribute("class", "ac_LC_colis");
+        nouveau_colis.setAttribute("id", "colis_"+i);
+        nouveau_colis.setAttribute("label", choix_type_colis[i]);
+        nouveau_colis.onclick = function() {
+            if (commande_id == null) {
+                status_creation_commande.id_colis = i;
+                creation_commande_p3(); 
+            }
+            else {
+                document.getElementById("type_cagette_commande_"+commande_id).value = this.getAttribute("label");
+                terminer_ajout(false);
+            }
+                       
+        }
+        let case_colis = document.createElement("input");
+        case_colis.setAttribute("type", "radio");
+        case_colis.setAttribute("name", "colis");
+        case_colis.setAttribute("class", "ac_case_colis");
+        case_colis.setAttribute("id", choix_type_colis[i]);
+    
+
+        let nom_colis = document.createElement("label");
+        nom_colis.setAttribute("for", choix_type_colis[i]);
+        nom_colis.setAttribute("class", "ac_label_colis");
+        nom_colis.innerHTML = choix_type_colis[i];
+    
+        nouveau_colis.appendChild(case_colis);
+        nouveau_colis.appendChild(nom_colis);
+    
+        main_part.appendChild(nouveau_colis);
+    }
+
+    // Coloration de l'input en mémoire (+ le coche) s'il en existe un.
+    if (commande_id != null) {
+        for (let i=0; i<choix_type_colis.length; i++) {
+            if (choix_type_colis[i] == document.getElementById("type_cagette_commande_"+commande_id).value) {
+                status_creation_commande.id_colis = i;
+                break;
+            }
+        }
+    }
+    if (status_creation_commande.id_colis != null) {
+        document.getElementById(choix_type_colis[status_creation_commande.id_colis]).checked = true;
+        document.getElementById("colis_"+status_creation_commande.id_colis).style.backgroundColor = "green";
+    }
+
+    // Affectation de la valeur onclick du bouton ac_bouton_retour_arriere.
+    if (commande_id == null) {
+        document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "creation_commande_p1()");
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler création";
+    }
+    else {
+        document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "terminer_ajout(false)");
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler";
+    }
+}
+
+// Création de l'interface pour choisir le nombre de palettes de la commande.
+function creation_commande_p3(commande_id=null) {
+    // Initialisation.
+    if (!status_creation_commande.estOuvert) {
+        document.getElementById("ajouter_commande").style.display = "block";
+        status_creation_commande.estOuvert = !status_creation_commande.estOuvert;
+    }
+    document.getElementById("ac_bouton_valider").style.display = "none";
+    document.getElementById("ac_main_ajout_commande").innerHTML = "";
+    document.getElementById("ac_titre_ajout_commande").innerHTML = "Nombre de palettes";
+
+    let main_part = document.getElementById("ac_main_ajout_commande");
+
+    // Création des boutons pour choisir le nombre de palettes.
+    for (let i=0; i<3; i++) {
+        let nouveau_bouton = document.createElement("button");
+        nouveau_bouton.setAttribute("class", "ac_bouton_palette");
+        nouveau_bouton.innerHTML = i;
+        nouveau_bouton.onclick = function() {
+            if (commande_id == null) {
+                status_creation_commande.nb_palettes = i;
+                document.getElementById("ajouter_commande").style.display = "none";
+                terminer_ajout(true);
+            }
+            else {
+                document.getElementById("nb_palettes_commande_"+commande_id).innerHTML = i;
+                terminer_ajout(false);
+            }
+        }
+        main_part.appendChild(nouveau_bouton);
+    }
+
+    // Affectation de la valeur onclick du bouton ac_bouton_retour_arriere.
+    if (commande_id == null) {
+        document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "creation_commande_p2()");
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler création";
+    }
+    else {
+        document.getElementById("ac_bouton_retour_arriere").setAttribute("onclick", "terminer_ajout(false)");
+        document.getElementById("ac_bouton_annuler").innerHTML = "annuler";
+    }
+}
+
